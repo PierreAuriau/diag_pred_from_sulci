83ea58597fb1d41b32e494214391df3d2e625dae
PierreAuriau <p.auriau@hotmail.fr>
2023-07-24 16:29:22 +0200

    remplace if by elif & add combination for wandb

--- augmentation/da_module.py
+++ augmentation/da_module.py
@@ -5,27 +5,34 @@ from preprocessing.transforms import Crop
 
 
 class DA_Module(object):
-
+    # FIXME : elif instead of if and else ValueError
+    # FIXME : add __str__ method
     def __init__(self, transforms=("cutout", "rotation")):
         self.compose_transforms = Transformer()
         for t in transforms:
             if t == "flip":
                 self.compose_transforms.register(flip, probability=0.5)
-            if t == "add_blur":
+            elif t == "add_blur":
                 self.compose_transforms.register(add_blur, probability=0.5, sigma=(0.1, 1))
-            if t == "add_noise":
+            elif t == "add_noise":
                 self.compose_transforms.register(add_noise, sigma=(0.1, 1), probability=0.5)
-            if t == "cutout":
+            elif t == "cutout":
                 self.compose_transforms.register(cutout, probability=0.5, patch_size=32, inplace=False)
-            if t == "Crop":
+            elif t == "Crop":
                 self.compose_transforms.register(Crop((96, 96, 96), "random", resize=True), probability=0.5)
-            if t == "rotation":
+            elif t == "rotation":
                 self.compose_transforms.register(rotation, angles=5, order=0, probability=0.5)
-            if t == "random_cutout":
+            elif t == "random_cutout":
                 self.compose_transforms.register(random_cutout, patch_size_ratio=0.4, min_size_ratio=0.1,
                                                  on_data=True, probability=1)
-            if t == "cutout_with_threshold":
+            elif t == "cutout_with_threshold":
                 self.compose_transforms.register(cutout_with_threshold, patch_sizes=[1, 52, 65, 52], probability=1)
+            elif t == "random_rotation_cutout":
+                self.compose_transforms.register(random_cutout, patch_size_ratio=0.4, min_size_ratio=0.1,
+                                                 on_data=True, probability=1)
+                self.compose_transforms.register(rotation, angles=5, order=0, probability=0.5)
+            else:
+                raise ValueError(f"Unknown data augmentation : {t}")
 
     def __call__(self, x):
         return self.compose_transforms(x)
