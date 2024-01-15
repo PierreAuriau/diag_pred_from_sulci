db99aa66ee0744d2bfee8daff7edc70e097d6c9f
PierreAuriau <p.auriau@hotmail.fr>
2024-01-15 19:14:08 +0100

    update preproc param and change training into run

--- datasets/datamanager.py
+++ datasets/datamanager.py
@@ -13,7 +13,7 @@ from torch.utils.data import DataLoader, SequentialSampler, RandomSampler
 from datasets.clinical_multisites import SCZDataset, BDDataset, ASDDataset
 from contrastive_learning.contrastive_datasets import ContrastiveSCZDataset, \
     ContrastiveBDDataset, ContrastiveASDDataset
-from img_processing.preprocessing import Padding, Crop, Normalize, Binarize
+from img_processing.preprocessing import Padding, Crop, Normalize, Binarize, GaussianSmoothing
 from img_processing.da_module import DAModule
 
 logger = logging.getLogger()
@@ -24,26 +24,30 @@ DataItem = namedtuple("DataItem", ["inputs", "outputs", "labels"])
 class ClinicalDataManager(object):
 
     def __init__(self, root: str, preproc: str, db: str, labels: List[str] = None, sampler: str = "random",
-                 batch_size: int = 1, number_of_trainings: int = None, mask=None,
+                 batch_size: int = 1, nb_runs: int = None, mask=None,
                  model: str = "base", data_augmentation: List[str] = None,
                  device: str = "cuda", **dataloader_kwargs):
 
         assert db in ["scz", "bd", "asd"], "Unknown db: %s" % db
         assert sampler in ["random", "sequential"], "Unknown sampler '%s'" % sampler
         assert model in [None, "base", "SimCLR", "SupCon", "y-aware"], f"Unknown model {model}"
+        assert preproc in [None, "no", "smoothing"]
 
         self.dataset = dict()
         self.labels = labels or []
         self.mask = mask
-        self.number_of_trainings = number_of_trainings
+        self.nb_runs = nb_runs
         self.sampler = sampler
         self.batch_size = batch_size
         self.device = device
         self.dataloader_kwargs = dataloader_kwargs
 
-        input_transforms = self.get_input_transforms(preproc=preproc, model=model,
-                                                     data_augmentation=data_augmentation)
+        input_transforms = self.get_input_transforms(preproc=preproc)
         logger.debug(f"input_transforms : {input_transforms}")
+        if data_augmentation is None:
+            data_augmentation = ("no", )
+        data_augmentation = DAModule(transforms=data_augmentation)
+        logger.debug(f"data_augmentation : {data_augmentation}")
         dataset_cls = None
         if db == "scz":
             if model == "SupCon":
@@ -61,16 +65,17 @@ class ClinicalDataManager(object):
             else:
                 dataset_cls = ASDDataset
         logger.debug(f"Dataset CLS : {dataset_cls}")
-        self.dataset["train"] = [dataset_cls(root, preproc=preproc, split="train",
-                                             transforms=input_transforms, target=labels)
-                                 for _ in range(self.number_of_trainings)]
-        self.dataset["validation"] = [dataset_cls(root, preproc=preproc, split="val",
-                                                  transforms=input_transforms, target=labels)
-                                      for _ in range(self.number_of_trainings)]
-        self.dataset["test"] = dataset_cls(root, preproc=preproc, split="test",
-                                           transforms=input_transforms, target=labels)
-        self.dataset["test_intra"] = dataset_cls(root, preproc=preproc, split="test_intra",
-                                                 transforms=input_transforms, target=labels)
+        dataset = dataset_cls(root, split="train",
+                              transforms=data_augmentation, target=labels)
+        dataset = dataset.transform(input_transforms, copy=False)
+        self.dataset["train"] = [dataset for _ in range(self.nb_runs)]
+        dataset = dataset_cls(root, split="val", target=labels)
+        dataset = dataset.transform(input_transforms, copy=False)
+        self.dataset["validation"] = [dataset for _ in range(self.nb_runs)]
+        for split in ["test", "test_intra"]:
+            dataset = dataset_cls(root, split=split, target=labels)
+            dataset = dataset.transform(input_transforms, copy=False)
+            self.dataset[split] = dataset
 
     @staticmethod
     def collate_fn(list_samples):
@@ -90,7 +95,7 @@ class ClinicalDataManager(object):
         return DataItem(**data)
 
     def get_dataloader(self, train=False, validation=False,
-                       test=False, test_intra=False, training_index=None):
+                       test=False, test_intra=False, run=None):
 
         assert test + test_intra <= 1, "Only one tests accepted"
         _test, _train, _validation, sampler = (None, None, None, None)
@@ -103,7 +108,7 @@ class ClinicalDataManager(object):
             tests_to_return.append("test_intra")
         test_loaders = dict()
         for t in tests_to_return:
-            dataset = self.dataset[t] if t != "validation" else self.dataset[t][training_index]
+            dataset = self.dataset[t] if t != "validation" else self.dataset[t][run]
             drop_last = True if len(dataset) % self.batch_size == 1 else False
             if drop_last and t != "validation":
                 logger.warning(f"The last subject will not be tested ! "
@@ -116,10 +121,10 @@ class ClinicalDataManager(object):
             test_loaders["test"] = test_loaders.pop("test_intra")
         if train:
             if self.sampler == "random":
-                sampler = RandomSampler(self.dataset["train"][training_index])
+                sampler = RandomSampler(self.dataset["train"][run])
             elif self.sampler == "sequential":
-                sampler = SequentialSampler(self.dataset["train"][training_index])
-            dataset = self.dataset["train"][training_index]
+                sampler = SequentialSampler(self.dataset["train"][run])
+            dataset = self.dataset["train"][run]
             drop_last = True if len(dataset) % self.batch_size == 1 else False
             _train = DataLoader(
                 dataset, batch_size=self.batch_size, sampler=sampler,
@@ -129,27 +134,16 @@ class ClinicalDataManager(object):
         return SetItem(train=_train, **test_loaders)
 
     @staticmethod
-    def get_input_transforms(preproc, model="base", data_augmentation=None):
-        if preproc in ["vbm", "quasi_raw"]:
-            # Input size 121 x 145 x 121
-            input_transforms = Compose(
-                [Crop((1, 121, 128, 121)), Padding([1, 128, 128, 128], mode='constant'), Normalize()])
-        elif preproc in ["skeleton"]:
-            # Input size 128 x 152 x 128
-            input_transforms = Compose(
+    def get_input_transforms(preproc):
+        # Input size 128 x 152 x 128
+        input_transforms = Compose(
                 [Padding([1, 128, 160, 128], mode='constant'), Binarize(one_values=[30, 35, 60, 100, 110, 120])])
-        else:
-            raise ValueError("Unknown preproc: %s" % preproc)
-        if model == "SupCon":
-            if data_augmentation is None:
-                input_transforms.transforms.append(DAModule())
-                logger.info("Data augmentation is set to the standard DA_model")
-            else:
-                input_transforms.transforms.append(DAModule(transforms=data_augmentation))
+        if preproc == "smoothing":
+                input_transforms.transforms.append(GaussianSmoothing(sigma=1.0, size=5))
         return input_transforms
 
-    def get_nb_trainings(self):
-        return self.number_of_trainings
+    def get_number_of_runs(self):
+        return self.nb_runs
 
     def __str__(self):
         return "ClinicalDataManager"
